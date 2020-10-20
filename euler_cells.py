b322f4eeedb66f4de66a9b3bf84043e536527607
Poker-person <vinades.hernandes@gmail.com>
2020-10-19 23:07:08 -0300

    asuhed8o

---
+++ euler_cells.py
@@ -0,0 +1,47 @@
+# https://projecteuler.net/problem=2
+
+# 1 - enconte a soma de todos os numeros naturais multiplos de 3 ou 5 ate 1000
+##
+multiplos = []
+
+for i in range(1, 1000):
+    if (i % 3 == 0) or (i % 5 == 0):
+        multiplos.append(i)
+
+print(sum(multiplos))
+
+# 2 - Encontre a soma dos numeros pares da sequencia de Fibonacci ate 4 milhoes
+##
+x1 = 0
+x2 = 0
+fibonacci_par = []
+
+for i in range(1, 40000):
+    x2 = x2 + x1
+    x1 = x2 - x1
+    if x2 == 0:
+        x2 = x2 + 1
+    if x1 % 2 == 0:
+        fibonacci_par.append(x1)
+
+print(sum(fibonacci_par))
+
+# 3 - What is the largest prime factor of the number 600851475143?
+##
+cu = []
+res = []
+
+for num in range(1, 600851475143):
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                break
+        else:
+            if (13195 % num) == 0:
+                res.append(num)
+
+print("O maior fator primo de 600851475143 é", res[-1])
+
+# 4 - A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers
+# is 9009 = 91 × 99. Find the largest palindrome made from the product of two 3-digit numbers.
+##
